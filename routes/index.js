1a8e36f90673345296984cff00cb3e7244fe9734
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-02-28 14:33:38 -0500

    Listing pages on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wikistack' });
+  db.Page.find(function (err, pages) {
+    if (err) throw err;
+    res.render('index', { title: 'Wikistack', docs: pages});
+  });
 });
 
 router.get('/about_us', function(req, res, next) {
