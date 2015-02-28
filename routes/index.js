01a49b2304a5ddec86ff22af9f0e20083a5e6c13
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-02-28 12:29:01 -0500

    Add styling for all pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wikistack' });
+});
+
+router.get('/addPage', function(req, res, next) {
+  res.render('addPage', { title: 'Add Page' });
+});
+
+router.get('/about_us', function(req, res, next) {
+  res.render('about_us', { title: 'About Us' });
+});
+
+router.get('/nav', function(req, res, next) {
+  res.render('nav', { title: 'About Us' });
 });
 
 module.exports = router;
