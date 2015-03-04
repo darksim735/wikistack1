5e0daaf28501090755a8a8a2041f64a00642d151
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-03-03 21:41:03 -0500

    Added class notes

--- routes/showPost.js
+++ routes/showPost.js
@@ -8,7 +8,7 @@ router.get('/:url', function (req, res) {
     });
   // url not matching (spaces)
   db.Page.findOne({'url_name': urlName}, function (err, page) {
-    console.log(urlName);
+    // console.log(urlName);
     res.render('show', {doc: page});
   })
 })
@@ -18,6 +18,8 @@ router.get('/:url/editPage', function(req, res, next) {
       return '%' + c.charCodeAt(0).toString(16);
     });
   // url not matching (spaces)
+  // could use add and pass property edit: true
+  // if editing - x code, else y code then endif
   db.Page.findOne({'url_name': urlName}, function (err, page) {
     res.render('editPage', {doc: page});
   })
@@ -25,18 +27,24 @@ router.get('/:url/editPage', function(req, res, next) {
 
 router.post('/:url/editPage/submit', function(req, res, next) {
   // if doc has _id 
-  var models = require('../models/');
   var title = req.body.title;
   var body = req.body.content;
   var urlName = encodeURIComponent(title).replace(/[!'()*]/g, function(c) {
       return '%' + c.charCodeAt(0).toString(16);
     });
-
+  // res render id as url
+  // when post /page/:id - res render urlname
   db.Page.findOneAndUpdate({'title': title}, {body: body, url_name: urlName}, {multi: true, upsert: true}, function (err, doc) {
     res.redirect('/wiki/'+urlName);
   })
 
 });
+
+router.post('/:url/deletePage', function (req, res) {
+  // if url was the id 
+  db.Page.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+})
 
 module.exports = router;
 
